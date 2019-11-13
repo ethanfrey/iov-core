5175e9150c355756531703756a9b84198af39844
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-11-13 20:02:36 +0100

    Start making bnsCodec more generic

--- packages/iov-weave/src/bnscodec.ts
+++ packages/iov-weave/src/bnscodec.ts
@@ -1,5 +1,7 @@
 import {
+  Address,
   ChainId,
+  Identity,
   Nonce,
   PostableBytes,
   PrehashType,
@@ -17,38 +19,57 @@ import { buildSignedTx, buildUnsignedTx } from "./encode";
 import * as codecImpl from "./generated/codecimpl";
 import { appendSignBytes, identityToAddress, isValidAddress } from "./util";
 
-export const bnsCodec: TxCodec = {
+// TODO: make this more general
+export interface TxType {
+  readonly encode: (message: codecImpl.bnsd.ITx) => protobuf.Writer;
+  readonly decode: (data: Uint8Array) => codecImpl.bnsd.Tx;
+}
+
+export class WeaveCodec<T extends TxType> implements TxCodec {
+  private readonly txType: T;
+
+  public constructor(txType: T) {
+    this.txType = txType;
+  }
+
   // these are the bytes we create to add a signature
   // they often include nonce and chainID, but not other signatures
-  bytesToSign: (tx: UnsignedTransaction, nonce: Nonce): SigningJob => {
+  public bytesToSign(tx: UnsignedTransaction, nonce: Nonce): SigningJob {
     // we encode it without any signatures
     const built = buildUnsignedTx(tx);
-    const bz = codecImpl.bnsd.Tx.encode(built).finish();
+    const bz = this.txType.encode(built).finish();
     // now we want to append the nonce and chainID
     const bytes = appendSignBytes(bz, tx.creator.chainId, nonce);
     return { bytes: bytes, prehashType: PrehashType.Sha512 };
-  },
+  }
 
   // bytesToPost includes the raw transaction appended with the various signatures
-  bytesToPost: (tx: SignedTransaction): PostableBytes => {
+  public bytesToPost(tx: SignedTransaction): PostableBytes {
     const built = buildSignedTx(tx);
-    const out = new Uint8Array(codecImpl.bnsd.Tx.encode(built).finish());
+    const out = new Uint8Array(this.txType.encode(built).finish());
     return out as PostableBytes;
-  },
+  }
 
   // identifier is usually some sort of hash of bytesToPost, chain-dependent
-  identifier: (tx: SignedTransaction): TransactionId => {
-    const transactionBytes = (bnsCodec.bytesToPost(tx) as unknown) as TxBytes;
+  public identifier(tx: SignedTransaction): TransactionId {
+    const transactionBytes = (this.bytesToPost(tx) as unknown) as TxBytes;
     const hash = v0_31.hashTx(transactionBytes);
     return Encoding.toHex(hash).toUpperCase() as TransactionId;
-  },
+  }
 
   // parseBytes will recover bytes from the blockchain into a format we can use
-  parseBytes: (bz: PostableBytes, chainId: ChainId): SignedTransaction => {
-    const parsed = codecImpl.bnsd.Tx.decode(bz);
+  public parseBytes(bz: PostableBytes, chainId: ChainId): SignedTransaction {
+    const parsed = this.txType.decode(bz);
     return parseTx(parsed, chainId);
-  },
+  }
 
-  identityToAddress: identityToAddress,
-  isValidAddress: isValidAddress,
-};
+  public identityToAddress(identity: Identity): Address {
+    return identityToAddress(identity);
+  }
+
+  public isValidAddress(address: string): boolean {
+    return isValidAddress(address);
+  }
+}
+
+export const bnsCodec = new WeaveCodec(codecImpl.bnsd.Tx);
--- packages/iov-weave/types/bnscodec.d.ts
+++ packages/iov-weave/types/bnscodec.d.ts
@@ -1,2 +1,28 @@
-import { TxCodec } from "@iov/bcp";
-export declare const bnsCodec: TxCodec;
+import {
+  Address,
+  ChainId,
+  Identity,
+  Nonce,
+  PostableBytes,
+  SignedTransaction,
+  SigningJob,
+  TransactionId,
+  TxCodec,
+  UnsignedTransaction,
+} from "@iov/bcp";
+import * as codecImpl from "./generated/codecimpl";
+export interface TxType {
+  readonly encode: (message: codecImpl.bnsd.ITx) => protobuf.Writer;
+  readonly decode: (data: Uint8Array) => codecImpl.bnsd.Tx;
+}
+export declare class WeaveCodec<T extends TxType> implements TxCodec {
+  private readonly txType;
+  constructor(txType: T);
+  bytesToSign(tx: UnsignedTransaction, nonce: Nonce): SigningJob;
+  bytesToPost(tx: SignedTransaction): PostableBytes;
+  identifier(tx: SignedTransaction): TransactionId;
+  parseBytes(bz: PostableBytes, chainId: ChainId): SignedTransaction;
+  identityToAddress(identity: Identity): Address;
+  isValidAddress(address: string): boolean;
+}
+export declare const bnsCodec: WeaveCodec<typeof codecImpl.bnsd.Tx>;
